6cc1f15b1247eb83d57b2c42eca7bec4ae0b9a97
Lucas Ou-Yang <lucasyangpersonal@gmail.com>
2014-03-01 21:23:20 -0800

    added instrucitons to readme

---
+++ shell.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2.7
+"""
+/shell.py will allow you to get a console and enter commands within your flask environment.
+"""
+import os
+import sys
+import readline
+from pprint import pprint
+
+from flask import *
+
+sys.path.insert(0, '/home/lucas/www/shorten.tv/shorten-env/shorten.tv')
+from backend import *
+
+os.environ['PYTHONINSPECT'] = 'True'
